d2439a341c78e45e0faa65e2f5358ff80595d7af
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2025-06-08 20:30:18 +0530

    CHG | Create Action modal

--- src/components/invoice/CreateInvoice.jsx
+++ src/components/invoice/CreateInvoice.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ActionModal from '../widgets/ActionModal'
 
 const CreateInvoice = () => {
   return (
     <div>
-      Create Invoice
+      <ActionModal>
+        
+      </ActionModal>
     </div>
   )
 }
