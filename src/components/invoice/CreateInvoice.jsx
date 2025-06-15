ac2a4fdbe0a6f4504e155c01a9ed367c6e470a38
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2025-06-15 22:40:15 +0530

    CHG | Create Invoice UI

--- src/components/invoice/CreateInvoice.jsx
+++ src/components/invoice/CreateInvoice.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import ActionModal from '../widgets/ActionModal'
+import { Button } from '../ui/button'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
 
 const CreateInvoice = () => {
+  const[open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values) {
+    console.log(values)
+  }
   return (
     <div>
-      <ActionModal>
-        
+      <ActionModal
+        title="Create Invoive"
+        desc="Create a new Invoice"
+        trigger={
+          <Button className="text-white space-x-1">
+            <span>Create Invoice</span>
+            <span className='text-lg'>+</span>
+          </Button>
+        }
+        open={open}
+        setOpen={setOpen}
+      >
+        <h1>Invoice</h1>
       </ActionModal>
     </div>
   )
--- src/components/ui/form.jsx
+++ src/components/ui/form.jsx
@@ -16,9 +16,9 @@ const FormField = (
   }
 ) => {
   return (
-    (<FormFieldContext.Provider value={{ name: props.name }}>
+    <FormFieldContext.Provider value={{ name: props.name }}>
       <Controller {...props} />
-    </FormFieldContext.Provider>)
+    </FormFieldContext.Provider>
   );
 }
 
@@ -51,9 +51,9 @@ const FormItem = React.forwardRef(({ className, ...props }, ref) => {
   const id = React.useId()
 
   return (
-    (<FormItemContext.Provider value={{ id }}>
+    <FormItemContext.Provider value={{ id }}>
       <div ref={ref} className={cn("space-y-2", className)} {...props} />
-    </FormItemContext.Provider>)
+    </FormItemContext.Provider>
   );
 })
 FormItem.displayName = "FormItem"
@@ -62,11 +62,11 @@ const FormLabel = React.forwardRef(({ className, ...props }, ref) => {
   const { error, formItemId } = useFormField()
 
   return (
-    (<Label
+    <Label
       ref={ref}
       className={cn(error && "text-destructive", className)}
       htmlFor={formItemId}
-      {...props} />)
+      {...props} />
   );
 })
 FormLabel.displayName = "FormLabel"
@@ -75,7 +75,7 @@ const FormControl = React.forwardRef(({ ...props }, ref) => {
   const { error, formItemId, formDescriptionId, formMessageId } = useFormField()
 
   return (
-    (<Slot
+    <Slot
       ref={ref}
       id={formItemId}
       aria-describedby={
@@ -84,7 +84,7 @@ const FormControl = React.forwardRef(({ ...props }, ref) => {
           : `${formDescriptionId} ${formMessageId}`
       }
       aria-invalid={!!error}
-      {...props} />)
+      {...props} />
   );
 })
 FormControl.displayName = "FormControl"
@@ -93,31 +93,31 @@ const FormDescription = React.forwardRef(({ className, ...props }, ref) => {
   const { formDescriptionId } = useFormField()
 
   return (
-    (<p
+    <p
       ref={ref}
       id={formDescriptionId}
       className={cn("text-sm text-muted-foreground", className)}
-      {...props} />)
+      {...props} />
   );
 })
 FormDescription.displayName = "FormDescription"
 
 const FormMessage = React.forwardRef(({ className, children, ...props }, ref) => {
   const { error, formMessageId } = useFormField()
-  const body = error ? String(error?.message) : children
+  const body = error ? String(error?.message ?? "") : children
 
   if (!body) {
     return null
   }
 
   return (
-    (<p
+    <p
       ref={ref}
       id={formMessageId}
       className={cn("text-sm font-medium text-destructive", className)}
       {...props}>
       {body}
-    </p>)
+    </p>
   );
 })
 FormMessage.displayName = "FormMessage"
