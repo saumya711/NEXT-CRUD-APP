972fb9344ad59f8692c8aca13a4d7af027c645e1
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2025-07-01 22:56:17 +0530

    CHG | Get Invoices - Component Logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+import { getInvoices } from "@/actions/InvoiceActions";
 import CreateInvoice from "@/components/invoice/CreateInvoice";
 import ListInvoice from "@/components/invoice/ListInvoice";
 import { Separator } from "@/components/ui/separator";
 
-export default function Home() {
+export default async function Home(searchParams) {
+
+  const search = searchParams?.search || "";
+  const page = searchParams?.page || "";
+
+  const res = await getInvoices({
+    search,
+    page,
+    limit: 5
+  });
+
+ const invoices = JSON.parse(res) || [];
+
+  console.log(invoices);
+
   return (
     <div className="flex justify-center min-h-[82vh]">
       <section className="w-full px-2 max-w-[1000 px]">
@@ -11,7 +26,11 @@ export default function Home() {
           <CreateInvoice />
         </div>
         <Separator className="my-2 border-b-[2px] border-color-light-blue"/>
-        <ListInvoice />
+        <ListInvoice
+          total={invoices.total}
+          pageNumber={invoices.pageCount}
+          invoices={invoices.data}
+        />
       </section>
     </div>
   );
